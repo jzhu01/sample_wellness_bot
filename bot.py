1eef145c6105161c44f1a082999769e700678e4c
Jen Zhu <jennifer0zhu@gmail.com>
2021-05-12 21:20:31 -0400

    modified the original echo bot code to welcome user and provide adaptive poll card

--- bot.py
+++ bot.py
@@ -1,21 +1,32 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
+import json
+import os
 
-from botbuilder.core import ActivityHandler, TurnContext
-from botbuilder.schema import ChannelAccount
+from botbuilder.core import ActivityHandler, TurnContext, CardFactory
+from botbuilder.schema import ChannelAccount, Attachment, Activity, ActivityTypes
 
-
+CARDS = [
+    "resources/EmotionPoll.json",
+]
 class MyBot(ActivityHandler):
-    # See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
-
-    async def on_message_activity(self, turn_context: TurnContext):
-        await turn_context.send_activity(f"You said '{ turn_context.activity.text }'")
 
     async def on_members_added_activity(
         self,
         members_added: ChannelAccount,
         turn_context: TurnContext
     ):
-        for member_added in members_added:
-            if member_added.id != turn_context.activity.recipient.id:
-                await turn_context.send_activity("Hello and welcome!")
+        for member in members_added:
+            if member.id != turn_context.activity.recipient.id:
+                message = Activity(
+                    text=f"Hello {member.name}!",
+                    type=ActivityTypes.message,
+                    attachments=[self._create_adaptive_card_attachment()],
+                )
+
+                await turn_context.send_activity(message)
+
+    def _create_adaptive_card_attachment(self) -> Attachment:
+        card_path = os.path.join(os.getcwd(), CARDS[0])
+        with open(card_path, "rb") as in_file:
+            card_data = json.load(in_file)
+
+        return CardFactory.adaptive_card(card_data)
